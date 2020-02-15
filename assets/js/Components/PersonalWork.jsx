2411d3ddb04d52cf14b2828517d8532bb5d639aa
mykidok <mickael.pegnin@gmail.com>
2020-02-15 15:08:45 +0100

    Add some personal work

--- assets/js/Components/PersonalWork.jsx
+++ assets/js/Components/PersonalWork.jsx
@@ -5,11 +5,13 @@ const PersonalWork = () => {
         <div className="mt-2">
             <div className="title mb-2">Personal Work&nbsp;<span role="img" aria-label="personal">🤙</span></div>
 
-            <p className="line">$&nbsp;<a href="http://www.my-football-bets.fr" className="text-white">My Football Bets</a></p>
+            <p className="line">$&nbsp;<a href="https://www.my-football-bets.fr" className="text-white">My Football Bets</a></p>
             <p className="line text-white">---->&nbsp;As I like to bet on football matches and I don't like losing time, I was tired to spend 2 hours every week to choose which match I will bet on</p>
             <p className="line text-white">---->&nbsp;So I developed a web application that do it for me</p>
             <p className="line text-white">---->&nbsp;It uses a self-made algorithm to evaluate how many goals a match will have and what the result will be </p>
             <p className="line text-white">---->&nbsp;Then it calculates on which matches I have the best chances to win and chooses the first two ones</p><br/>
+            <p className="line">$&nbsp;<a href="https://www.eloisecarville.com" className="text-white">Eloïse Carville Traduction</a></p>
+            <p className="line text-white">---->&nbsp;A self-entrepreneur colleague wanted a website for her translation/interpretation society to win visibility</p><br/>
         </div>
     )
 };
